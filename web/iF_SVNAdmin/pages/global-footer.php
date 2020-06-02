c6ab140a555c6b4b91e0544e9dc0e7fbb8463d47
goalnmail@gmail.com <goalnmail@gmail.com>
2020-06-02 10:44:31 +0800

    remove pages url

--- web/iF_SVNAdmin/pages/global-footer.php
+++ web/iF_SVNAdmin/pages/global-footer.php
@@ -2,10 +2,10 @@
     </div><!-- end: contentarea -->
 
 	  <div id="bottomarea">
-	    <p>
+	    <!--p>
 	      <a href="http://www.insanefactory.com/if-svnadmin/?pk_campaign=iFSVNAdmin-Footer" target="_blank">iF.SVNAdmin <?php AppVersion(); ?></a> -
 	      &copy; 2009-2012 <a href="http://www.insanefactory.com/?pk_campaign=iFSVNAdmin-Footer" target="_blank">insaneFactory.com</a>
-	    </p>
+	    </p-->
 	  </div>
 
   </div><!-- End id=main -->
